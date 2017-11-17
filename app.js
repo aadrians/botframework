1ff165b4559b0c71749c14d25e4458e3b966c8c1
arya <arya.adriansyah@gmail.com>
2017-11-17 09:35:30 +0100

    fix index.html

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var builder = require('botbuilder');
 // Setup Restify Server
 var server = restify.createServer();
 
-server.listen()
-
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
@@ -28,7 +26,7 @@ var bot = new builder.UniversalBot(connector, function (session) {
 });
 
 // Serve a static web page
-server.get(/.*/, restify.serveStatic({
+server.get(/.*/, restify.plugins.serveStatic({
 	'directory': '.',
 	'default': 'index.html'
 }));
