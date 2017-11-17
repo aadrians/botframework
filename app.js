f14d3b344f61080ec7607aef1bd52478d5d83a71
arya <arya.adriansyah@gmail.com>
2017-11-17 10:05:44 +0100

    fix bot

--- app.js
+++ app.js
@@ -30,20 +30,3 @@ server.get(/.*/, restify.plugins.serveStatic({
 	'directory': '.',
 	'default': 'index.html'
 }));
-
-// FB page ID: 127971741225929
-// URL: https://www.facebook.com/TPA-Musihoven-127971741225929/
-
-// From FB
-// API version 2.11
-// APP ID: 688967961295116 
-// App secret: 793c8b9df68eb70ff199175a0324f121
-
-//Facebook App
-// Page token for TPA Musihoven: EAAJynNqYOQwBAJBZAGmc9HJQ4TpG5ieAiCrscZBj6ZBkO4HwELNMQZA01ZAOQYq27TzKB94yfIHYOOcZBKujocZB25VMarHinOZCFVcZA1LXED5bhRu9NacvVuQP9DhubiWiQZAio1soPS4wZCxfazT73KF1vKGhUpUIv2T10rvWMoS3gZDZD
-
-
-// registering app
-// App name: AryaTestBot
-// App ID: d3dd73d2-d826-4df1-a5d1-b58144b5c072
-// App password: wwpjTXNZ19-*&dyuHNL670!
